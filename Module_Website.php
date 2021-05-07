13dec78d36b87dfbe0cc15751a964714f97859fa
gizmore <gizmore@wechall.net>
2021-05-07 22:21:42 +0200

    6.10.2-r1078

---
+++ Module_Website.php
@@ -0,0 +1,16 @@
+<?php
+namespace GDO\Website;
+
+use GDO\Core\GDO_Module;
+
+/**
+ * Setup metadata for your GDT_Page.
+ * @link https://www.matuzo.at/blog/html-boilerplate/
+ * @author gizmore
+ * @version 6.10.2
+ * @since 6.10.2
+ */
+final class Module_Website extends GDO_Module
+{
+    
+}
