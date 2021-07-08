12994f3deb462fc9d82ea1e6a59c58426bb32249
Christian Busch <busch@bmpgmbh.de>
2021-07-08 15:41:17 +0200

    6.10.4-r1176

--- Module_Website.php
+++ Module_Website.php
@@ -7,16 +7,20 @@ use GDO\Core\GDO_Module;
  * Setup metadata for your GDT_Page.
  * @link https://www.matuzo.at/blog/html-boilerplate/
  * @author gizmore
- * @version 6.10.2
+ * @version 6.10.4
  * @since 6.10.2
  */
 final class Module_Website extends GDO_Module
 {
+    public $module_priority = 95;
+    
     public function onLoadLanguage() { return $this->loadLanguage('lang/website'); }
 
     public function getConfig()
     {
-        return [];
+        return [
+            
+        ];
     }
     
 }
